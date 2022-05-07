f61344e3ac0e2285ba5bfae47a69333ce21d554d
bzamora020 <bryan.aldair.zamora@gmail.com>
2022-05-07 12:31:09 -0700

    template to render images

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def home():
     if form.validate_on_submit():
         file = form.file.data # First grab the file
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename))) # Then save the file
-        return "File has been uploaded."
+        return  render_template('response.html', form=form)
     return render_template('index.html', form=form)
 
 if __name__ == '__main__':
